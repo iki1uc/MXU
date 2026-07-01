0a67e71e4f434e74876eed2d0003bc144795c26f
iki1uc <entertrainer@web.de>
2026-07-01 15:53:07 +0200

    Update forward.js

--- AX/forward.js
+++ AX/forward.js
@@ -1,4 +1,12 @@
 export function AX_forward() {
-    return "AX → forward() OK";
-}
+    console.log("AX → forward()");
 
+    return {
+        id: "AX",
+        type: "forward",
+        status: "OK",
+        level: 1,
+        timestamp: Date.now(),
+        info: "AX → Forward-Move erfolgreich ausgeführt"
+    };
+}
