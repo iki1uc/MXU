c42b0151b92a8bd019e90f55225c7b7b2f9f9292
iki1uc <entertrainer@web.de>
2026-07-01 15:46:12 +0200

    Update iu.js

--- IU/iu.js
+++ IU/iu.js
@@ -1,1 +1,10 @@
+export function UX_move() {
+    console.log("UX → move()");
+    return {
+        id: "UX",
+        type: "move",
+        status: "OK",
+        info: "UX → Move erfolgreich ausgeführt"
+    };
+}
 
