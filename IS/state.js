b4f53a5004b3c3050e3feea37cd198028d312fde
iki1uc <entertrainer@web.de>
2026-07-01 15:53:28 +0200

    Refactor IS_state function to return object

--- IS/state.js
+++ IS/state.js
@@ -1,4 +1,12 @@
 export function IS_state() {
-    return "IS → state() OK";
-}
+    console.log("IS → state()");
 
+    return {
+        id: "IS",
+        type: "state",
+        status: "OK",
+        level: 2,
+        timestamp: Date.now(),
+        info: "IS → State-Move erfolgreich ausgeführt"
+    };
+}
