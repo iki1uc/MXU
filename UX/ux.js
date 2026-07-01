525ff8fe4d959d388488f72e1688d1fe0ad1bfe6
iki1uc <entertrainer@web.de>
2026-07-01 15:45:16 +0200

    Add MX_move function to UX/ux.js

--- UX/ux.js
+++ UX/ux.js
@@ -1,1 +1,9 @@
-
+export function MX_move() {
+    console.log("MX → move()");
+    return {
+        id: "MX",
+        type: "move",
+        status: "OK",
+        info: "MX → Move erfolgreich ausgeführt"
+    };
+}
