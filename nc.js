6998ddffd964263a8ffc42981321d484ddc8a258
iki1uc <entertrainer@web.de>
2026-07-02 04:05:41 +0200

    Update nc.js

--- nc.js
+++ nc.js
@@ -1,1 +1,9 @@
+export function ncImprove(history) {
+  // Beispiel: einfache Verbesserung
+  const last = history[history.length - 1];
+  return {
+    suggestion: "next-level",
+    basedOn: last
+  };
+}
 
