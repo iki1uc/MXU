63ef681cff096647719d537a2596dc131c56d0eb
iki1uc <entertrainer@web.de>
2026-07-03 19:40:05 +0200

    Update MXU_fromPX.js

--- MXU_fromPX.js
+++ MXU_fromPX.js
@@ -1,1 +1,12 @@
+import { PXGateSync } from "./PXGateSync.js";
+
+const px = new PXGateSync();
+
+export function MXU_fromPX(msg) {
+    const pxOut = px.sync(msg);
+    return {
+        px: pxOut,
+        status: "PX→MXU OK"
+    };
+}
 
