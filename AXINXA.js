8cd9d3cfd54bb927d5e687c4b57dddf56a00ed7f
iki1uc <entertrainer@web.de>
2026-07-20 23:57:14 +0200

    Update AXINXA.js

--- AXINXA.js
+++ AXINXA.js
@@ -1,30 +1,22 @@
-import { AX_forward } from "./AX/forward.js";
-import { IS_state } from "./IS/state.js";
-import { XA_return } from "./XA/return.js";
-
-import { MX_move } from "./MX/mx.js";
-import { UX_move } from "./UX/ux.js";
-import { IU_move } from "./IU/iu.js";
-
 export const AXINXA = {
 
     attach(mxu) {
-        console.log("AXINXA → an MXU gekoppelt");
         this.mxu = mxu;
-        this.runAll();
-        this.runNew();
     },
 
     runAll() {
-        console.log("AX:", AX_forward());
-        console.log("IS:", IS_state());
-        console.log("XA:", XA_return());
+        return {
+            AX: AX_forward(),
+            IS: IS_state(),
+            XA: XA_return()
+        };
     },
 
     runNew() {
-        console.log("MX:", MX_move());
-        console.log("UX:", UX_move());
-        console.log("IU:", IU_move());
+        return {
+            MX: MX_move(),
+            UX: UX_move(),
+            IU: IU_move()
+        };
     }
 };
-
