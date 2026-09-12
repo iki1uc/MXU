25c2f24b7932d058e989b050a3c464b0fabfda56
iki1uc <entertrainer@web.de>
2026-09-12 16:17:14 +0200

    Add tmp81 property to dyn object in evoImprove

--- nc.evo.js
+++ nc.evo.js
@@ -1,6 +1,11 @@
 export function evoImprove(history = []) {
   const last = history[history.length - 1] || {};
 
+  const isTMP81 =
+    last.mode === "or3" ||
+    last.mode === "or9" ||
+    last.mode === "or81";
+
   const dyn = {
     bewegung: last.atom?.dir || "none",
     anker: last.atom?.stable || false,
@@ -9,7 +14,8 @@ export function evoImprove(history = []) {
     grav: last.atom?.grav || { dir: "none", force: 0 },
     zug: last.atom?.pull || 0,
     abstoss: last.atom?.push || 0,
-    singular: last.atom?.singularity || false
+    singular: last.atom?.singularity || false,
+    tmp81: isTMP81
   };
 
   return {
