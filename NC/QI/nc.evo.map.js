7ab7ebbcb0d9e5bbecb91294dd0a93c577ac4230
iki1uc <entertrainer@web.de>
2026-09-12 20:29:40 +0200

    Update nc.evo.map.js

--- NC/QI/nc.evo.map.js
+++ NC/QI/nc.evo.map.js
@@ -1,1 +1,77 @@
+// NC → EVO-MAP (leicht, frei, vollständig)
+
+export const NC_EVO_MAP = {
+
+    "◉": {
+        name: "origin",
+        axis: "X",
+        depth: 0,
+        height: 0,
+        layer: 0
+    },
+
+    "3": {
+        name: "start",
+        axis: "Y",
+        depth: 1,
+        height: 0,
+        layer: 1
+    },
+
+    "9": {
+        name: "end",
+        axis: "Z",
+        depth: 1,
+        height: 0,
+        layer: 2
+    },
+
+    "◎": {
+        name: "core",
+        axis: "O/H",
+        depth: 2,
+        height: 0,
+        layer: 3
+    },
+
+    "◆": {
+        name: "root",
+        axis: "X",
+        depth: 3,
+        height: 0,
+        layer: 0
+    },
+
+    "△": {
+        name: "layer",
+        axis: "X",
+        depth: 0,
+        height: 1,
+        layer: 1
+    },
+
+    "27": {
+        name: "height",
+        axis: "X",
+        depth: 0,
+        height: 2,
+        layer: 2
+    },
+
+    "▣": {
+        name: "space",
+        axis: "X",
+        depth: 1,
+        height: 1,
+        layer: 3
+    },
+
+    "↺": {
+        name: "repeat",
+        axis: "X",
+        depth: 0,
+        height: 0,
+        layer: 0
+    }
+};
 
